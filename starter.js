3be65b003a9475ee120115542944880af59d5cf7
michael shamah <shamah.michael@gmail.com>
2016-06-08 14:01:55 -0400

    puesdo code for JS and css and html basics.

---
+++ starter.js
@@ -0,0 +1,31 @@
+//make a function to accept the input from the user
+  //put in the chatbox
+
+//make a function to create a number from the computer
+  //put these things in the chatbox
+
+//make a function that compares the two number and gives an out come
+  // if same number return what the hit was
+  // if pitcher was higher than batter give out
+  //if batter was higher than pitch give strike
+  //have all these things reflect on the scoreboard
+  //put thee things in the chatbox
+
+//make a function that retur the number of bases the runners move
+  //add the hit (single=1, double=2,triple=3, homerun=4) to the baserunners
+  // if the the base number is 4 or higher add a score
+  //else put the runners at the right base
+  //have it reflect on the scoreboard and field
+
+//make a function that switches batter and pitcher
+  //every three outs switch the batter and pitcher
+  //have it refelct on the scoreboard
+
+
+//make function(or if else statment) that checks strikes and return if out or not
+  //have ut reflect on socreboard
+
+//make a function that checks to see who won
+  //after three innings check
+  //who ever has more runs they win
+  //alert the user know
